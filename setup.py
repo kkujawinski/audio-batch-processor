ae17c13e579db377094d52e38c29906ad1551805
Kamil Kujawinski <kamil@kujawinski.net>
2019-05-27 23:40:36 +0200

    adb 0.3.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name='audio-batch-processor',
-    version='0.2.0',
+    version='0.3.0',
     license='GNU GPLv3',
     description='Batch tool for processing audio files.',
     long_description='%s\n%s' % (read('README.rst'), re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))),
